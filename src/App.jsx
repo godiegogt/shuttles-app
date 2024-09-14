ce500cd41fe831da960ee786f69cce9069d0103e
LepusX1 <handelmontalvo@gmail.com>
2024-09-14 16:29:44 -0600

    added imports to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
+import { useState } from 'react'
 import './App.css'
 import './routes/Main'
-
+import Main from './routes/Main'
 import Admin from './routes/admin/Admin'
-
+import SignIn from './routes/signIn/SignIn'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-   <Admin/>
+   <SignIn/>
   )
 }
 
