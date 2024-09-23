030bf7cc75c0f089baf4e02ce2e306b6fcf7d3ee
katra47 <marlonamilcar20@gmail.com>
2024-09-23 14:26:45 -0600

    scroll

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,30 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Layouts/Header/Header';
 import Card from '../Layouts/Card/Card';
-import { Box, TextField, Button } from '@mui/material';
+import { Box } from '@mui/material';
+import useShuttle from '../hooks/useShuttle'; // Ensure the path is correct
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function Main() {
-  // Estado para los valores dinámicos
-  const [placa, setPlaca] = useState("123BCfD");
-  const [status, setStatus] = useState("En Ruta");
-  const [from, setFrom] = useState("Coban");
-  const [to, setTo] = useState("Carcha");
-  const [estimatedTime, setEstimatedTime] = useState(90);
+  const { shuttles } = useShuttle();
+  const [visibleShuttles, setVisibleShuttles] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    // Initially load a certain number of shuttles
+    setVisibleShuttles(shuttles.slice(0, 10)); // Adjust the number as needed
+  }, [shuttles]);
+
+  const fetchMoreData = () => {
+    if (visibleShuttles.length >= shuttles.length) {
+      setHasMore(false);
+      return;
+    }
+    
+    // Load more shuttles
+    setVisibleShuttles(shuttles.slice(0, visibleShuttles.length + 10)); // Load 10 more
+  };
 
   return (
     <div>
       <Header />
-      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', mt: 4 }}>
-        <Card 
-          placa={placa} 
-          status={status} 
-          from={from} 
-          to={to} 
-          estimatedTime={estimatedTime} 
-        />
-      </Box>
+      <InfiniteScroll
+        dataLength={visibleShuttles.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+      >
+        <Box display={'flex'} flexDirection={'row'} flexWrap={'wrap'} justifyContent={'flex-start'}>
+          {visibleShuttles.length === 0 ? (
+            <p>No hay shuttles disponibles.</p>
+          ) : (
+            visibleShuttles.map(shuttle => (
+              <Card 
+                key={shuttle.id} 
+                placa={shuttle.plateNumber} 
+                status={shuttle.currentState} 
+                from={shuttle.origin} 
+                to={shuttle.destination} 
+              />
+            ))
+          )}
+        </Box>
+      </InfiniteScroll>
     </div>
   );
 }
 
-export default Main;0
+export default Main;
